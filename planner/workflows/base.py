460a06501dbf3ecb5588a0e777d41a3704b7114a
saizhu13 <saizhu13@gmail.com>
2026-09-23 16:03:54 +0800

    Add protocol comments

--- planner/workflows/base.py
+++ planner/workflows/base.py
@@ -3,7 +3,10 @@ from typing import Protocol
 from planner.domain.mission import MissionStage
 from planner.domain.result import StageResult
 
-
+'''
+任何对象只要想作为 Workflow 使用，就必须提供一个 run() 方法。
+这个方法接收一个任务阶段 MissionStage，返回该阶段的执行结果 StageResult
+'''
 class Workflow(Protocol):
     def run(self, stage: MissionStage) -> StageResult[object]: ...
 
